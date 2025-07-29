aad804a7317d7320fa9679e1012f9778b4e619ea
ejder123 <omersaideser@gmail.com>
2025-07-29 21:57:58 +0300

    Update config.js

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ module.exports = {
   TOKEN: "",
   language: "en",
   ownerID: ["1004206704994566164", ""], 
-  mongodbUri : "",
+  mongodbUri : "mongodb+srv://Kamera01:Kamera01@discordbotu.rnzbgxe.mongodb.net/?retryWrites=true&w=majority",
   spotifyClientId : "",
   spotifyClientSecret : "",
   setupFilePath: './commands/setup.json',
